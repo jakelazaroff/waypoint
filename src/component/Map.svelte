eb1a0d2963e477364aadab81cc93be978009113e
Jake Lazaroff <yo@jake.nyc>
2024-01-24 00:19:30 -0500

    Use map layers for pins

--- src/component/Map.svelte
+++ src/component/Map.svelte
@@ -57,16 +57,24 @@
         }
       });
 
+      const pin = new Image();
+      pin.src = "/pin.svg";
+      pin.onload = () => map.addImage("pin", pin);
+
       map.addLayer({
         id: "points",
         type: "symbol",
         source: "places",
         layout: {
+          "icon-image": "pin",
+          "icon-anchor": "bottom",
+          "icon-allow-overlap": true,
           "text-field": ["get", "name"],
-          // "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+          "text-font": ["Overpass", "Open Sans Semibold", "Arial Unicode MS Bold"],
           "text-size": 12,
           "text-offset": [0, 0.5],
-          "text-anchor": "top"
+          "text-anchor": "top",
+          "text-optional": true
         }
       });
     });
@@ -103,18 +111,12 @@
     const source = map.getSource("places") as mapboxgl.GeoJSONSource | undefined;
     source?.setData({ type: "FeatureCollection", features: places });
 
-    const markers = places.map(place =>
-      new mapboxgl.Marker().setLngLat(place.geometry.coordinates).addTo(map)
-    );
-
     const ids = new Set(places.map(place => place.properties.mapboxId));
     if (ids.size !== placeids.size || intersection(placeids, ids).size !== ids.size) {
       placeids = ids;
       const { ne, sw } = boundingBox(places.map(place => place.geometry.coordinates));
       map.fitBounds([ne, sw], { padding: 200, maxZoom: 12, duration: 1000 });
     }
-
-    return () => markers.forEach(marker => marker.remove());
   });
 </script>
 
